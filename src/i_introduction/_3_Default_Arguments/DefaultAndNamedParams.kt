9de40ce56aefe76abeecbd6f74ea70a481d8b266
Matthias Lambrecht <mats.lan.pod@googlemail.com>
2017-06-24 00:10:48 +0900

    Solved Koan 03

--- src/i_introduction/_3_Default_Arguments/DefaultAndNamedParams.kt
+++ src/i_introduction/_3_Default_Arguments/DefaultAndNamedParams.kt
@@ -14,11 +14,20 @@ fun todoTask3(): Nothing = TODO(
     documentation = doc2(),
     references = { name: String -> JavaCode3().foo(name); foo(name) })
 
-fun foo(name: String, number: Int = 42, toUpperCase: Boolean = false): String = todoTask3()
+fun foo(name: String, number: Int = 42, toUpperCase: Boolean = false): String {
+    if (toUpperCase) {
+        return (name + number).toUpperCase()
+    }
+    return name + number
+}
 
 fun task3(): String {
     return (foo("a") +
             foo("b", number = 1) +
             foo("c", toUpperCase = true) +
             foo(name = "d", number = 2, toUpperCase = true))
+
+
+
 }
+
